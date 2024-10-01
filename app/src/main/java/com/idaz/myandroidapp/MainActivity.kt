378615e5e47d513fc7d9d1d1baf5924e69fe31f2
idazanggara <idazideas@gmail.com>
2024-10-01 15:45:48 +0700

    Fix Android Kotlin 01-Layout

--- app/src/main/java/com/idaz/myandroidapp/MainActivity.kt
+++ app/src/main/java/com/idaz/myandroidapp/MainActivity.kt
@@ -8,9 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var textView: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
